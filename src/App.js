1f7d31388bb3aaaf33c5bf0ec76e66eb5418ab1c
Juliana Quintero <juli.carmesi@gmail.com>
2023-04-18 22:37:44 -0500

    testing paths

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route
-            path="/PreEntrega2-Quintero--React"
-            element={<ItemListContainer />}
-          />
+          <Route path="/" element={<ItemListContainer />} />
           <Route path="/category/:categoryId" element={<ItemListContainer />} />
           <Route path="/item/:itemId" element={<ItemDetailContainer />} />
           <Route path="*" element={<h1>404 NOT FOUND</h1>} />
